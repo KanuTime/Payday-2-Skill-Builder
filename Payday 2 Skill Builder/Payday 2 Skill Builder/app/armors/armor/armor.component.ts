9eaabe42760299030a3c391a7800341e0f0bbaf7
Kanu <kanu.privat@gmail.com>
2016-06-13 22:54:15 +0200

    Added some armor

---
+++ Payday 2 Skill Builder/Payday 2 Skill Builder/app/armors/armor/armor.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'armor',
+    templateUrl: './armor.component.html'
+})
+export class Armor {
+
+}
---
+++ Payday 2 Skill Builder/Payday 2 Skill Builder/app/armors/armors.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'armors',
+    templateUrl: './armors.component.html'
+})
+export class Armors {
+
+
+
+}
